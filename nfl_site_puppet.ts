e14332748d86e792acfb6e826c0296132f00f93e
tkravits <taylor.kravits@gmail.com>
2021-12-30 17:16:28 -0700

    doing some code cleanup

--- nfl_site_puppet.ts
+++ nfl_site_puppet.ts
@@ -1,23 +1,28 @@
-const puppeteer = require('puppeteer');
-let nfl_url: string = "https://fantasy.nfl.com/research/pointsagainst";
+const puppeteer = require('puppeteer')
+const nflUrl: string = 'https://fantasy.nfl.com/research/pointsagainst'
 
-async function scrape_table(url: string) {
-  const browser = await puppeteer.launch({ headless: false, 
-    slowMo: 250, defaultViewport: null});
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
+async function scrapeTable (url: string) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250,
+    defaultViewport: null
+  })
+  const page = await browser.newPage()
+  await page.goto(url, { waitUntil: 'networkidle2' })
   const result = await page.evaluate(() => {
-      const rows = document.querySelectorAll('table tr');
-      return Array.from(rows, row => {
-        const columns = row.querySelectorAll('td');
-        return Array.from(columns, column => (column as HTMLElement).innerText);
-      });
-    });
-  
-    
-    await browser.close();
-    return result;
+    const rows = document.querySelectorAll('table tr')
+    return Array.from(rows, row => {
+      const columns = row.querySelectorAll('td')
+      return Array.from(columns, column => (column as HTMLElement).textContent)
+    })
+  })
+  await browser.close()
+  return result
 }
 
-const nfl_results = scrape_table(nfl_url);
-console.log(nfl_results);
+// const nflResults = await scrapeTable(nflUrl)
+// console.log(nflResults)
+(async function () {
+  const nflResults = await scrapeTable(nflUrl)
+  console.log(nflResults)
+})()
