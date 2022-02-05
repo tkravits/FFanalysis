0209f45b07992e9edd4fbd693a83657513a12e17
tkravits <taylor.kravits@gmail.com>
2022-02-04 22:51:53 -0700

    feat: getting something built without react firsdt

--- src/nfl_site_puppet.ts
+++ src/nfl_site_puppet.ts
@@ -1,10 +1,41 @@
-//const puppeteer = require('puppeteer')
-//const nflUrl: string = 'https://fantasy.nfl.com/research/pointsagainst'
+require('dotenv').config();
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import puppeteer from 'puppeteer';
+const nflWebsite = {
+  url: "https://fantasy.nfl.com/research/pointsagainst",
+};
+let results: string[][] = [];
+
+// Config variables
+const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
+process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\n/g, '\n')
+const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+const appendSpreadsheet = async (row) => {
+ 
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: CLIENT_EMAIL,
+      private_key: PRIVATE_KEY,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+
+    const sheet = doc.sheetsById[SHEET_ID];
+    const result = await sheet.addRow(row);
+    console.log(row.type)
+  } catch (e) {
+    console.error('Error: ', e);
+  }
+};
 
 async function scrapeTable (url: string) {
   const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 250,
+    headless: true,
+    //slowMo: 10,
     defaultViewport: null
   })
   const page = await browser.newPage()
@@ -20,9 +51,15 @@ async function scrapeTable (url: string) {
   return result
 }
 
-// const nflResults = await scrapeTable(nflUrl)
-// console.log(nflResults)
+
 (async function () {
-  const nflResults = await scrapeTable(nflUrl)
-  console.log(nflResults)
-})()
+  console.log(nflWebsite.url)
+  results = await scrapeTable(nflWebsite.url)
+  console.log(results)
+  for (let i of results) {
+    //let team = i[0]
+    // need to define either the array more specifically or figure out how to define i
+    let team = results[i][0]
+    appendSpreadsheet(team)
+}
+})();
